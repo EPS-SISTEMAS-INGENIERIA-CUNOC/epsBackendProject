75b2dd5d04f475e590efd5617d964f675d41a67f
edvinteodoro <egonzales@digitalgeko.com>
2021-08-31 20:01:46 -0600

    agregar configuraciones keycloak

---
+++ src/main/java/gt/edu/cunoc/epsBackend/config/BeanKeycloakConfiguration.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gt.edu.cunoc.epsBackend.config;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ *
+ * @author teodoro
+ */
+
+@Configuration
+public class BeanKeycloakConfiguration {
+    @Bean
+    public KeycloakSpringBootConfigResolver KeycloakConfigResolver() {
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
--- src/main/java/gt/edu/cunoc/epsBackend/controller/HelloWorldController.java
+++ src/main/java/gt/edu/cunoc/epsBackend/controller/HelloWorldController.java
@@ -5,6 +5,7 @@
  */
 package gt.edu.cunoc.epsBackend.controller;
 
+import javax.annotation.security.RolesAllowed;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +18,19 @@ public class HelloWorldController {
     
     @RequestMapping("/")
     public String hello() {
-        return "Hola Mundo validando Cache!!";
+        return "Hola Mundo Todos!!";
+    }
+    
+    @RequestMapping("/user")
+    @RolesAllowed("backend-user")
+    public String helloUser() {
+        return "Hola Mundo User!!";
+    }
+    
+    @RequestMapping("/admin")
+    @RolesAllowed("backend-admin")
+    public String helloAdmin() {
+        return "Hola Mundo Admin!!";
     }
     
 }
